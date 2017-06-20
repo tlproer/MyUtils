2f57713440cff0b10cea8aa238e2e0cb50513daf
tlproer <sjy870416@sina.com>
2017-06-20 22:47:43 +0800

    commit

---
+++ src/main/java/com/tanglei/espresso/EspressoApplication.java
@@ -0,0 +1,12 @@
+package com.tanglei.espresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspressoApplication.class, args);
+	}
+}
